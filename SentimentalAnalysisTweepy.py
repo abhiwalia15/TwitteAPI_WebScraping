8778c1af0a2ff47fe5507f2609d9efb397c05452
Mrinal <waliamrinal@gmail.com>
2018-10-18 13:12:09 +0530

    add folloing cchnages

--- SentimentalAnalysisTweepy.py
+++ SentimentalAnalysisTweepy.py
@@ -1,7 +1,7 @@
 import re
 import tweepy
 from tweepy import OAuthHandler
-#from textblob import Textblob
+import textblob 
 
 class TwitterCliend(object):
 	'''generic twitter class for sentimental analysis'''
@@ -33,7 +33,23 @@ class TwitterCliend(object):
 		''' Utility function to clean tweet text by removing links, special characters 
         using simple regex statements. '''
 		return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet).split())
-     
+		
+	def get_tweet_sentiment(self, tweet):
+		'''utility function to classify sentiment of passed tweet using textblob's sentiment method'''
+		
+		#create TEXTBLOB object of passes tweet text
+		analysis = textblob(self.clean_tweet(tweet))
+		#set sentiment
+		if analysis.sentiment.polarity > 0:
+			return 'positive'
+		elif analysis.sentiment.polarity == 0:
+			return 'neutral'
+		else:
+			return 'negative'
+			
+	
+			
+	
                                     
                                     
                                     
