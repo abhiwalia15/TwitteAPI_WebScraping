42e27f06e6dfe2aad5b26af7d2d2611ea3731d4f
Mrinal <waliamrinal@gmail.com>
2018-10-18 16:55:17 +0530

    add thsi hsit

--- SentimentalAnalysisTweepy.py
+++ SentimentalAnalysisTweepy.py
@@ -96,24 +96,27 @@ def main():
 	#percentage of positive tweets
 	print("Positive tweets percentage : {}%".format(100*len(ptweets)/len(tweets)))
     
-    #picking negative tweets from tweets
-    ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
-    #percentage of negative tweets
-    print("Negative tweets percentage : {}%".format(100*len(ntweets)/len(tweets)))
+	#picking negative tweets from tweets	
+	ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
+	#percentage of negative tweets
+	print("Negative tweets percentage : {}%".format(100*len(ntweets)/len(tweets)))
     
-    #percentage of neutral tweets
-    print("Neutral tweets percentage: {}% \".format(100*len(tweets - ntweets - ptweets)/len(tweets)))
+	#percentage of neutral tweets
+	print("Neutral tweets percentage: {} % \ ".format(100*len(tweets - ntweets - ptweets)/len(tweets))) 
     
-    #printing first 5 positive tweets
-    print("\n\nPositive tweets:")
-    for tweet in ptweets[:10]:
+	#printing first 5 positive tweets
+	print("\n\nPositive tweets:")
+	for tweet in ptweets[:10]:
 		print(tweet['text'])                                
                              
-    #printing first 5 negative tweets
-    print("\n\nNegative tweets:")
-    for tweet in ntweets[:10]:
+	#printing first 5 negative tweets
+	print("\n\nNegative tweets:")
+	for tweet in ntweets[:10]:
 		print(tweet['text'])        
-                                    
+                             
+if __name__ == '__main__':
+	#calling main function
+	main()       
                                     
                                     
                                     
