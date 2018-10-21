7b9efd8377c03927ad759421bb24e542cf77f4ab
Mrinal <waliamrinal@gmail.com>
2018-10-21 13:34:01 +0530

    How To Create a Twitterbot with Python 3 and the Tweepy Library

---
+++ TwitterBot/credentials.py
@@ -0,0 +1,5 @@
+# keys and tokens from the Twitter Dev Console 
+consumer_key = 'H09lz1MRceBRko8xt86OIlv5i'
+consumer_secret = 'MtDWYPzXUxhEXfL5HQcKSSig0ncynEprZfyTJ7hIezHISWSOTa'
+access_token = '1050006838366810112-vayi2L8Q1vxbPbUOdibmw7CYQhb72g'
+access_token_secret = 'V4cCohGfJa30T8agz5LgStpNUqDetXbEmWjjSaiC8bpJc'
