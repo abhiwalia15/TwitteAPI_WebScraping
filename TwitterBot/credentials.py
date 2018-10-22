81be0715ac4e6c556c49d756808619ffcfd91e10
Mrinal <waliamrinal@gmail.com>
2018-10-23 03:59:38 +0530

    keys and tokens from the Twitter Dev Console

--- TwitterBot/credentials.py
+++ TwitterBot/credentials.py
@@ -1,4 +1,5 @@
 # keys and tokens from the Twitter Dev Console 
+# create an app in twitter and get these keys
 consumer_key = 'H09lz1MRceBRko8xt86OIlv5i'
 consumer_secret = 'MtDWYPzXUxhEXfL5HQcKSSig0ncynEprZfyTJ7hIezHISWSOTa'
 access_token = '1050006838366810112-vayi2L8Q1vxbPbUOdibmw7CYQhb72g'
